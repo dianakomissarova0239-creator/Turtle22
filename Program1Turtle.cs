ece75ffdd66d999c0ec34192daacb119bca7f491
Diana <Diana@lab>
2025-09-10 14:19:41 +0300

    כל המספרים שלמים בלי שארית. נוח לעשות רנדום עם המספרים אלו.

--- Program1Turtle.cs
+++ Program1Turtle.cs
@@ -9,17 +9,21 @@ namespace Turtle22
 {
     internal partial class Program
     {
+        public static Random rnd = new Random();
         /// <summary>
         /// Turtle program
         /// </summary>
         static void Main1()
         {
+            double x1, y1;
             Turtle t = new Turtle();
-
+            x1 = t.GetXLocation();
+            Console.WriteLine(x1);
             //t1.MoveBackward(200);
             t.SetDelay(20);
             t.TailDown();
             Double step = 100 * 2 * Math.PI / 36;
+            t.TurnRight(5);
             for (int i = 0; i < 36; i++)
             {
                 t.MoveForward(step);
@@ -28,15 +32,41 @@ namespace Turtle22
            
 
             }
+            t.TurnLeft(5);
+
+            x1 = Math.Round(t.GetXLocation(),2);
+            Console.WriteLine(x1);
+
             t.TurnRight(180);
-            t.MoveForward(100-step/2);
+            t.MoveForward(100);// -step/2);
             t.TurnRight(180);
             //t.MoveForward(100);
             for (int i = 0; i < 4; i++)
             {
                 t.MoveForward(200);
                 t.TurnRight(90);
+                double x = rnd.NextDouble() * 200;
+                double y = rnd.NextDouble() * 200;
+                t.TailUp();
+                t.MoveForward(y);
+                t.TurnRight(90);
+                t.MoveForward(x);
+                t.TailDown();
+                t.MoveForward(10);
+
+
+                x1 = Math.Round(t.GetXLocation(), 2);
+                y1 = Math.Round(t.GetYLocation(), 2);
+                Console.WriteLine($"x1:{x1}, y1: {y1}");
+
+
+
             }
+
+
+
+            
+
 
         }
     }
